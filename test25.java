6dbce9d218accd7b1775c5e1d031351f312b5d9f
rayshaaidaazalia <raysa4872@gmail.com>
2025-10-01 22:16:58 +0700

    mengganti

--- test25.java
+++ test25.java
@@ -3,6 +3,6 @@ public class test25 {
     public static void main(String[] args) {
         System.out.println("hai ini baris pertama");
     System.out.println("ini user 1 menambahkan baris");
-    System.out.println("ini user kedua menambahkan baris baru");
+    System.out.println(" user kedua menambahkan baris baru");
     }
 }
